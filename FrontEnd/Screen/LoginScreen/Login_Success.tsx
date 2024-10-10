9b9da737f019909f96b23de7fb2f3ed69397d90b
YoungJae01 <tlscard@naver.com>
2024-10-10 18:09:56 +0900

    카카오 로그인 구현

--- FrontEnd/Screen/LoginScreen/Login_Success.tsx
+++ FrontEnd/Screen/LoginScreen/Login_Success.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { View, Button, ToastAndroid } from 'react-native';
+import { View, Button, Alert,ToastAndroid } from 'react-native';
+import { login } from '@react-native-seoul/kakao-login';
 
 export const Google_PopUp = () => {
     ToastAndroid.showWithGravity(
@@ -17,12 +18,16 @@ export const Apple_PopUp = () => {
     );
 };
 
-export const Kakao_PopUp = () => {
-    ToastAndroid.showWithGravity(
-        "카카오 계정으로 로그인 되었습니다.",
-        ToastAndroid.SHORT,
-        ToastAndroid.BOTTOM
-    );
+export const Kakao_PopUp = async (): Promise<void> => {
+    try {
+        const result = await login();
+        console.log('로그인 성공:', result);
+        Alert.alert('로그인 성공', JSON.stringify(result));
+      } catch (error: unknown) {
+        console.error('로그인 실패:', error);
+        const errorMessage = error instanceof Error ? error.message : '알 수 없는 오류 발생';
+        Alert.alert('로그인 실패', errorMessage);
+      }
 };
 
 export const Guest_PopUp = () => {
