da3c8ecdeb69a0c55c6968f22baa2c97b4de31cb
YoungJae01 <tlscard@naver.com>
2024-10-10 20:07:29 +0900

    Feat : 카카오 로그인 구현 (설정 포함)

--- FrontEnd/Screen/LoginScreen/Login_Success.tsx
+++ FrontEnd/Screen/LoginScreen/Login_Success.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { View, Button, Alert,ToastAndroid } from 'react-native';
-import { login } from '@react-native-seoul/kakao-login';
+import { login, getProfile } from '@react-native-seoul/kakao-login';
 
 export const Google_PopUp = () => {
     ToastAndroid.showWithGravity(
@@ -12,7 +12,7 @@ export const Google_PopUp = () => {
 
 export const Apple_PopUp = () => {
     ToastAndroid.showWithGravity(
-        "애플 계정으로 로그인 되었습니다.",
+        "애플 계정로그인 구현 예정",
         ToastAndroid.SHORT,
         ToastAndroid.BOTTOM
     );
@@ -20,15 +20,36 @@ export const Apple_PopUp = () => {
 
 export const Kakao_PopUp = async (): Promise<void> => {
     try {
-        const result = await login();
-        console.log('로그인 성공:', result);
-        Alert.alert('로그인 성공', JSON.stringify(result));
-      } catch (error: unknown) {
-        console.error('로그인 실패:', error);
-        const errorMessage = error instanceof Error ? error.message : '알 수 없는 오류 발생';
-        Alert.alert('로그인 실패', errorMessage);
-      }
-};
+      // 로그인 시도
+      const result = await login();
+      console.log('로그인 성공:', result);  // 로그인 성공 로그 출력
+  
+      // 엑세스 토큰
+      const accessToken = result.accessToken;
+  
+      // 프로필 정보 가져오기
+      const profile = await getProfile();
+      console.log('사용자 프로필:', profile);  // 사용자 프로필 로그 출력
+  
+      // 이메일과 닉네임
+      const email = profile.email ? profile.email : '이메일 정보가 없습니다.';
+      const nickname = profile.nickname;
+      
+      // 모든 정보를 한 번에 출력
+      console.log('로그인 정보:', {
+        accessToken: accessToken,
+        email: email,
+        nickname: nickname,
+      });
+  
+      Alert.alert('로그인 성공', `닉네임: ${nickname}, 이메일: ${email}`);
+  
+    } catch (error: unknown) {
+      console.error('로그인 실패:', error);
+      const errorMessage = error instanceof Error ? error.message : '알 수 없는 오류 발생';
+      Alert.alert('로그인 실패', errorMessage);
+    }
+  };
 
 export const Guest_PopUp = () => {
     ToastAndroid.showWithGravity(
