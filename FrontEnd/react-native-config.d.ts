d1e25c8145e7472d62af4afa648ec6713a204fca
DESKTOP-E66LQ6K\봉가은 <pongaun@naver.com>
2024-11-15 21:50:14 +0900

    feat: 작업내용 반영

--- FrontEnd/react-native-config.d.ts
+++ FrontEnd/react-native-config.d.ts
@@ -2,6 +2,7 @@
 declare module 'react-native-config' {
   interface Env {
     AUTH_SERVER_URL: string; // AUTH_SERVER_URL 타입 정의 추가
+    KAKAO_APP_KEY : string; // KAKAO_APP_KEY 타입 정의 
   }
 
   const Config: Env;
