5f7b043f2c5a94e6df795f785c52ecf0fa4e3094
YunDaeHyeon <kayuaao96@gmail.com>
2024-11-15 21:36:21 +0900

    feat: 관심사 UI 테스트

--- FrontEnd/Screen/MyPage/MyPage.tsx
+++ FrontEnd/Screen/MyPage/MyPage.tsx
@@ -3,6 +3,8 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, Image, View, TouchableOpacity, Text} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NavigationBar} from '../Commonness/NavigationBar';
+import {initializeTtsListeners, playTTS} from '../../initializeTtsListeners';
+import {handleTestComponent} from '../../Function/Navigation';
 
 const MyPage = ({navigation}) => {
   const [nickname, setNickname] = useState('');
@@ -23,7 +25,8 @@ const MyPage = ({navigation}) => {
       }
     };
 
-    loadData();
+    loadData(); // kakao 프로필 불러오기
+    initializeTtsListeners(); // TTS 최초 설정
   }, []);
 
   return (
@@ -70,7 +73,11 @@ const MyPage = ({navigation}) => {
               source={require('../../Image/light.png')}
               style={Styles.setting_button_icon}
             />
-            <Text style={Styles.setting_button_text}> 밝기 조절</Text>
+            <Text
+              style={Styles.setting_button_text}
+              onPress={() => handleTestComponent(navigation)}>
+              테스트 페이지 이동
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
