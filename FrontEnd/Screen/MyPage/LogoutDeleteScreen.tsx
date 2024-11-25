df647081c919a15d92276ae7583c423deff73fb4
YoungJae01 <tlscard@naver.com>
2024-11-25 20:34:39 +0900

    Feat: 로직 수정 및 디자인 수정

--- FrontEnd/Screen/MyPage/LogoutDeleteScreen.tsx
+++ FrontEnd/Screen/MyPage/LogoutDeleteScreen.tsx
@@ -194,15 +194,8 @@ const styles = StyleSheet.create({
     width: '80%',
     backgroundColor: 'white',
     borderRadius: 10,
-    padding: 0,
+    padding: 20,
     alignItems: 'center',
-    height: '30%',
-  },
-  modalsubContainer: {
-    width: '100%',
-    height: '90%',
-
-    borderRadius: 10,
   },
   confirmModalSubContainer: {
     width: '100%',
